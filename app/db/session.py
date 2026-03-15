5c570839539c64e3cd7c059a865042abd412ce39
sallahvoice <sallahvoice@gmail.com>
2026-03-15 12:23:47 +0100

    database session file defining connection and local session

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,6 @@
 from app.core.config import settings
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = settings.db_url
 
@@ -14,5 +14,3 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
-
-base = declarative_base()
