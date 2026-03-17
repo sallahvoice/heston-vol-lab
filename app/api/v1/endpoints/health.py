8e70cbb4f9d141b55d25e6c87b78b6e03dc00a02
sallahvoice <sallahvoice@gmail.com>
2026-03-17 17:41:31 +0100

    chore: basic post request endpoints, calls service layer math functions

--- app/api/v1/endpoints/health.py
+++ app/api/v1/endpoints/health.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from datetime import datetime
 
-router = APIRouter(prefix="simulation", tags=["simulation"])
+router = APIRouter()
 
 @router.get("/health")
 def health():
--- app/api/v1/endpoints/simulation.py
+++ app/api/v1/endpoints/simulation.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 from app.services.brownian import (simulate_brownian_motion, simulate_correlated_brownian_motion)
 from app.services.gbm import simulate_gbm_paths, simulate_gbm_antihetic, simulate_gbm_euler 
@@ -7,42 +8,79 @@ from app.services.heston import simulate_heston_paths
 router = APIRouter(prefix="simulation", tags=["simulation"])
 
 
-@router.post("/monte_carlo")
-def simulate_monte_carlo(): #pass correct params
-    #call correct function
-    #make sure to return summary (to many paths)
-    pass
-
-
 @router.post("/brownian")
-def simulate_brownian():
-    pass
+def simulate_brownian(
+    n_steps: int,
+    n_paths: int,
+    T: float,
+    seed: int | None = None
+) -> Tuple[np.ndarray, np.ndarray]:
+    return simulate_brownian_motion(n_steps, n_paths, T, seed)
 
 
 @router.post("/correlated_brownian")
-def simulate_corr_brownian():
-    pass
+def simulate_corr_brownian(
+    rho,
+    n_steps: int,
+    n_paths: int,
+    T: float,
+    seed: int | None = None
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    return simulate_correlated_brownian_motion(rho, n_steps, n_paths, T, seed)
 
 
-@router.post("/gmb_price")
-def simulate_gbm_price_paths():
-    pass
+@router.post("/gbm_price")
+def simulate_gbm_price_paths(
+    S0: float,
+    mu: float,
+    sigma:  float,
+    T: float,
+    n_steps: int,
+    n_paths: int,
+    seed: int | None = None
+) -> np.ndarray:
+    return simulate_gbm_paths(S0, mu, sigma, T, n_steps, n_paths, seed)
 
 
 @router.post("/antihetic_gbm_price")
-def simulate_atihetic_gmb_price_paths():
-    pass
+def simulate_antihetic_gmb_price_paths(
+    S0: float,
+    mu: float,
+    sigma: float,
+    T: float,
+    n_steps: int,
+    n_paths: int,
+    seed: int | None = None
+) -> np.ndarray:
+    return simulate_gbm_antihetic(S0, mu, sigma, T, n_steps, n_paths, seed)
 
 
 @router.post("/gmb_euler_price")
-def simulate_euler_gbm_price_paths():
-    pass
+def simulate_euler_gbm_price_paths(
+    S0: float,
+    mu: float,
+    sigma: float,
+    T: float,
+    n_steps: int,
+    n_paths: int,
+    seed: int | None = None
+) -> np.ndarray:
+    return simulate_gbm_euler(S0, mu, sigma, T, n_steps, n_paths, seed)
 
-@router.post("/gbm")
-def simulate_gmb():
-    pass
 
 
 @router.post("/heston")
-def simulate_heston():
-    pass
+def simulate_heston_price_paths(
+    S0: float,
+    v0: float,
+    rho: float,
+    T: float,
+    n_steps: int,
+    n_paths: int,
+    mu: float,
+    theta: float,
+    kappa: float,
+    xi: float,
+    seed: int | None = None
+) -> tuple[np.ndarray, np.ndarray]:
+    return simulate_heston_paths(S0, v0, rho, T, n_steps, n_paths, mu, theta, kappa, xi, seed)
