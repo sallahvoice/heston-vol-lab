c9f128b3123d7066f86b21ec162b0f41c2e9251a
sallahvoice <sallahvoice@gmail.com>
2026-03-17 17:59:32 +0100

    included simulation router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints.health import health
+from app.api.v1.endpoints import health
+from app.api.v1.endpoints.simulation import router as simulation_router
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, tags=["health"]) #will include other endpoints
+api_router.include_router(simulation_router)
