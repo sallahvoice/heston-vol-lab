bad8685233ee71f1bf07d2f9f8c9202fd17f0609
sallahvoice <sallahvoice@gmail.com>
2026-03-19 14:47:24 +0100

    duplicated function name call/call, dict key named 'call_price' is weird considering the user is asking for it so 'price' is better

--- app/api/v1/endpoints/pricing.py
+++ app/api/v1/endpoints/pricing.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.utils.misc import _as_list
 
 from app.services.fft_pricing import carr_madan_fft_price
-from app.services.monte_carlo import (monte_carlo_european_call, monte_carlo_european_put)
+from app.services.monte_carlo import monte_carlo_european_call, monte_carlo_european_put
 
 from app.schemas.pricing import (
     CarrMadanRequest, CarrMadanResponse,
@@ -14,14 +14,12 @@ router = APIRouter(prefix="/pricing", tags=["pricing"])
 
 @router.post("/monte_carlo_call", response_model=MonteCarloResponse)
 def monte_carlo_call_pricing(req: MonteCarloRequest) -> MonteCarloResponse:
-    price = monte_carlo_european_call(req.St, req.K, req.r, req.T)
-    return {"call_price": price}
+    return {"price": monte_carlo_european_call(req.St, req.K, req.r, req.T)}
 
 
 @router.post("/monte_carlo_put", response_model=MonteCarloResponse)
-def monte_carlo_call_pricing(req: MonteCarloRequest) -> MonteCarloResponse:
-    price = monte_carlo_european_put(req.St, req.K, req.r, req.T)
-    return {"put_price": price}
+def monte_carlo_put_pricing(req: MonteCarloRequest) -> MonteCarloResponse:
+    return {"price": monte_carlo_european_put(req.St, req.K, req.r, req.T)}
 
 
 @router.post("/carr_madan", response_model=CarrMadanResponse)
